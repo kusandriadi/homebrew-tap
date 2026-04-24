4c38acfb121172b666098928cfee66c1d1eebc3e
Kus Andriadi <me@kusandriadi.com>
2026-04-24 13:49:50 +0700

    Apple Silicon only, remove Intel support

--- Casks/text-plus.rb
+++ Casks/text-plus.rb
@@ -1,21 +1,14 @@
 cask "text-plus" do
   version "1.0.0"
+  sha256 :no_check
 
-  on_arm do
-    sha256 :no_check
-    url "https://github.com/kusandriadi/text-plus/releases/download/v#{version}/text-plus_#{version}_aarch64.dmg"
-  end
-
-  on_intel do
-    sha256 :no_check
-    url "https://github.com/kusandriadi/text-plus/releases/download/v#{version}/text-plus_#{version}_x86_64.dmg"
-  end
-
+  url "https://github.com/kusandriadi/text-plus/releases/download/v#{version}/text-plus_#{version}_aarch64.dmg"
   name "Text Plus"
   desc "Lightweight code editor for macOS — Notepad++ alternative"
   homepage "https://github.com/kusandriadi/text-plus"
 
   depends_on macos: ">= :catalina"
+  depends_on arch: :arm64
 
   app "text-plus.app"
 
