c4717b76ee89eae93eb229aa5d8eeff0bebb73dd
Kus Andriadi <me@kusandriadi.com>
2026-04-24 13:50:32 +0700

    Remove _aarch64 from DMG filename

--- Casks/text-plus.rb
+++ Casks/text-plus.rb
@@ -2,7 +2,7 @@ cask "text-plus" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/kusandriadi/text-plus/releases/download/v#{version}/text-plus_#{version}_aarch64.dmg"
+  url "https://github.com/kusandriadi/text-plus/releases/download/v#{version}/text-plus_#{version}.dmg"
   name "Text Plus"
   desc "Lightweight code editor for macOS — Notepad++ alternative"
   homepage "https://github.com/kusandriadi/text-plus"
